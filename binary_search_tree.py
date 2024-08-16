442c21643d549f0798c341e5651ae53a8e2ed03e
EuniceMusenyia <eunicemusenyia@gmail.com>
2024-08-16 22:31:03 +0300

    Added binary search code

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -25,3 +25,49 @@ class BinarySearchTree:
                 root.right = Node(book)
             else:
                 self._insert(root.right, book)
+    def search(self, title):
+        return self._search(self.root, title)
+
+    def _search(self, root, title):
+        if root is None or root.book.title == title:
+            return root
+        if title < root.book.title:
+            return self._search(root.left, title)
+        return self._search(root.right, title)
+
+    def inorder_traversal(self):
+        elements = []
+        self._inorder_traversal(self.root, elements)
+        return elements
+
+    def _inorder_traversal(self, root, elements):
+        if root:
+            self._inorder_traversal(root.left, elements)
+            elements.append(root.book.title)
+            self._inorder_traversal(root.right, elements)
+
+    def delete(self, title):
+        self.root = self._delete(self.root, title)
+
+    def _delete(self, root, title):
+        if root is None:
+            return root
+        if title < root.book.title:
+            root.left = self._delete(root.left, title)
+        elif title > root.book.title:
+            root.right = self._delete(root.right, title)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            temp = self._min_value_node(root.right)
+            root.book = temp.book
+            root.right = self._delete(root.right, temp.book.title)
+        return root
+
+    def _min_value_node(self, root):
+        current = root
+        while current.left is not None:
+            current = current.left
+        return current
